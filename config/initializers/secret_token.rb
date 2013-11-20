87e0f9371dfd4acd8377bee09c03061822ee2f07
ozagy <bc2521@columbia.edu>
2013-11-20 17:39:00 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eventalent::Application.config.secret_key_base = '247d1e556529f04f177e67d1207da8da87ce33091858a06ec2541f14c9110bc357f5a743199c0157b7b4f37212c0854b9c62dc972bca7d17d71aac48a99a651b'
