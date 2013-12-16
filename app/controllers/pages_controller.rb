9f646656c836087de8f94892d328458173cf5bf8
ozagy <bc2521@columbia.edu>
2013-12-15 21:50:45 -0500

    finish function implementation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -99,10 +99,26 @@ class PagesController < ApplicationController
       @connections.each do |c|
         graph.connect_mutually( c[:uid1], c[:uid2], 1 )
       end
+      adj = adjacency(graph, @connections)
       @pos = forcedirected(graph, 0.1, 80000, 500, 1000)
-      @eig = eigenvector(graph, @connections)
+      @eig = eigenvector(graph, adj)
       @deg = degree(graph)
       @dis = graph.dijkstra(res['me'][0]['uid'])
+      clu_graph = Graph.new
+      clu_other = []
+      graph.each do |v|
+        if @deg[v] > 0
+          clu_graph.push v
+        else
+          clu_other.push v
+        end
+      end
+      @clu = bisection(clu_graph, degree(clu_graph), adjacency(clu_graph, @connections))
+      l = @clu.values[0].length
+      clu_other.each do |v|
+        @clu[v] = Array.new(l){0}
+      end
+      logger.info @clu
     end
     render layout: "none"
   end
@@ -151,6 +167,16 @@ class PagesController < ApplicationController
     return pos
   end
   
+  def adjacency(g, connections)
+    hash = Hash[g.map {|v| [v,g.index(v)]}]
+    adj_arr = Array.new(g.size){ Array.new(g.size){ 0 } }
+    connections.each do |c|
+      adj_arr[hash[c[:uid1]]][hash[c[:uid2]]] = 1
+      adj_arr[hash[c[:uid2]]][hash[c[:uid1]]] = 1
+    end
+    return Matrix.rows(adj_arr)
+  end
+  
   def degree(g)
     deg = {}
     g.each do |vertex|
@@ -159,14 +185,7 @@ class PagesController < ApplicationController
     return deg
   end
   
-  def eigenvector(g, connections)
-    hash = Hash[g.map {|v| [v,g.index(v)]}]
-    adj_arr = Array.new(g.size){ Array.new(g.size){ 0 } }
-    connections.each do |c|
-      adj_arr[hash[c[:uid1]]][hash[c[:uid2]]] = 1
-      adj_arr[hash[c[:uid2]]][hash[c[:uid1]]] = 1
-    end
-    adj = Matrix.rows(adj_arr)
+  def eigenvector(g, adj)
     v, d, vi = adj.eigensystem
     eig_v = v.column(g.size - 1)
     eig = {}
@@ -174,5 +193,61 @@ class PagesController < ApplicationController
       eig[vertex] = eig_v.[](g.index(vertex)).round(2)
     end
     return eig
+  end
+  
+  def bisection(g, deg_v, adj)
+    hash = Hash[g.map {|v| [v,g.index(v)]}]
+    deg_arr = Array.new(g.size){ Array.new(g.size){ 0 } }
+    g.each do |v|
+      deg_arr[hash[v]][hash[v]] = deg_v[v]
+    end
+    deg = Matrix.rows(deg_arr)
+    lap = deg - adj
+    v, d, vi = lap.eigensystem
+    logger.info v.t()
+    logger.info d
+    clu = {}
+    sign = v.[](0,0)
+    v.each_with_index do |e, row, col|
+      clu[g[row]] = [] if clu[g[row]].nil?
+      clu[g[row]][col] = (e*sign > 0) ? 0:1
+    end
+    c_new = {}
+    clu.each do |id, c_arr|
+      c_arr_new = []
+      i = 0
+      c_arr.each do |c|
+        if i < 5
+          c_arr_new[i] = c_arr.take(i+1).join('').to_i(2)
+        end
+        i = i+1
+      end
+      c_new[id] = c_arr_new
+    end
+    clu = c_new
+    clu[g[0]].length.times do |i|
+      arr = {}
+      clu.each do |id, c|
+        arr[id] = c[i]
+      end
+      arr = arr.sort_by {|k,v| v}
+      min = -1
+      num = 0
+      arr_new = {}
+      arr.each do |v|
+        if v[1] > min
+          num = num+1
+          arr_new[v[0]] = num
+          min = v[1]
+        else
+          arr_new[v[0]] = num
+          min = v[1]
+        end
+      end
+      g.each do |id|
+        c_new[id][i] = arr_new[id]
+      end
+    end
+    return c_new
   end
 end
